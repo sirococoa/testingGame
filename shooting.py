e1413dd71aadcb2c8e579cca25b9a346022ea798
sirococoa <torakia.mcml178@gmail.com>
2020-07-17 21:11:40 +0900

    [add] Playerクラスの追加
    Playerクラスの雛型を定義

--- shooting.py
+++ shooting.py
@@ -14,3 +14,14 @@ class Shooting:
 
     def draw(self):
         pass
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
