b007fdd8fee5bfc26bd8f38145a10a08cdd6b8b2
sirococoa <torakia.mcml178@gmail.com>
2020-07-20 23:11:23 +0900

    [add] particle管理クラスの追加

--- pyxel_tools.py
+++ pyxel_tools.py
@@ -25,3 +25,38 @@ def center(text, width):
     '''
     TEXT_W = 4
     return width // 2 - len(text) * TEXT_W // 2
+
+
+class ParticleSystem:
+    particles = []
+
+    @classmethod
+    def update(cls):
+        for p in ParticleSystem.particles:
+            p.update()
+        ParticleSystem.particles = [p for p in ParticleSystem.particles if p.time > 0]
+
+    @classmethod
+    def draw(cls):
+        for p in ParticleSystem.particles:
+            p.draw()
+
+    class Particle:
+        def __init__(self, x, y, u, v, w, h, time, colkey=None):
+            self.x = x
+            self.y = y
+            self.u = u
+            self.v = v
+            self.w = w
+            self.h = h
+            self.time = time
+            self.colkey = colkey
+
+        def update(self):
+            self.time -= 1
+
+        def draw(self):
+            if self.colkey:
+                pyxel.blt(self.x - self.w/2, self.y - self.h/2, 0, self.u, self.v, self.w, self.h, self.colkey)
+            else:
+                pyxel.blt(self.x - self.w/2, self.y - self.h/2, 0, self.u, self.v, self.w, self.h)
