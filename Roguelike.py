9f91b89cd3f235f36dc141b2866ab109ec4b8512
sirococoa <torakia.mcml178@gmail.com>
2020-07-23 02:08:54 +0900

    [add] RogueLikeクラスの雛型作成

---
+++ Roguelike.py
@@ -0,0 +1,16 @@
+import pyxel
+
+
+class RogueLike:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+if __name__ == '__main__':
+    RogueLike()
