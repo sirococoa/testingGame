a66e56e2635a9061dc33dc8b55f8a7b8091f9cd0
sirococoa <torakia.mcml178@gmail.com>
2020-07-29 16:25:08 +0900

    [add] Stageクラスの雛型作成

--- Roguelike.py
+++ Roguelike.py
@@ -12,5 +12,18 @@ class RogueLike:
         pass
 
 
+class Stage:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.make_stage()
+
+    def make_stage(self):
+        pass
+
+    def draw(self):
+        pass
+
+
 if __name__ == '__main__':
     RogueLike()
